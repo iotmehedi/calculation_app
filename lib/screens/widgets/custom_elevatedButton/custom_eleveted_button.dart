3112d1c5d1b20ca3c4ad9b058133d815a44977f8
Md. Mehedi Hasan <mdmehedihasan4051@gmail.com>
2024-03-22 12:42:12 +0600

    bmi calculator donee

---
+++ lib/screens/widgets/custom_elevatedButton/custom_eleveted_button.dart
@@ -0,0 +1,27 @@
+import 'package:calculation_app/core/utils/consts/app_colors.dart';
+import 'package:calculation_app/core/utils/consts/textstyle.dart';
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class CustomElevatedButton extends StatelessWidget {
+  final Color? color;
+  final HexColor? hexColor;
+  final VoidCallback? onPress;
+  final text;
+  const CustomElevatedButton(
+      {super.key, this.color, this.onPress, this.hexColor, this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+          backgroundColor: hexColor ?? color,
+          minimumSize: Size(MediaQuery.of(context).size.width, 60),
+          maximumSize: Size(MediaQuery.of(context).size.width, 60),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(8))),
+      onPressed: onPress,
+      child: text,
+    );
+  }
+}
